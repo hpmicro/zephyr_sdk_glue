b191062b5fcc219dfcd0aade9bbbbc4f708e31ea
Yingda WU <yingda.wu@hpmicro.com>
2024-03-26 18:46:52 +0800

    version: update version name
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

--- cmake/gen_version_h.cmake
+++ cmake/gen_version_h.cmake
@@ -2,4 +2,4 @@
 
 cmake_minimum_required(VERSION 3.13.0)
 
-configure_file(${HPM_ZEPHYR_DIR}/hpm_zephyr_version.h.in ${OUT_FILE})
+configure_file(${HPM_ZEPHYR_DIR}/sdk_glue_version.h.in ${OUT_FILE})

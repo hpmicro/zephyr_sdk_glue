2b67f91fc4a0c743339f118e3cb18fdc8973ea1e
Yingda WU <yingda.wu@hpmicro.com>
2024-03-26 19:40:49 +0800

    license: update license
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

--- modules/modules.cmake
+++ modules/modules.cmake
@@ -1,5 +1,5 @@
-# Copyright (c) 2023 HPMicro
-# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2024 HPMicro
+# SPDX-License-Identifier: Apache-2.0
 
 set(ZEPHYR_HPM_SDK_CMAKE_DIR  ${CMAKE_CURRENT_LIST_DIR}/hpm_sdk)
 set(ZEPHYR_HPM_SDK_KCONFIG    ${CMAKE_CURRENT_LIST_DIR}/hpm_sdk/Kconfig)

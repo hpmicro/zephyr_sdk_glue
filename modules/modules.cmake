7bcf6948e174e172c0d1b566e887dbcbf628bdeb
Yingda WU <yingda.wu@hpmicro.com>
2024-03-26 17:47:32 +0800

    modules: add hpm_sdk module
    - add hpm_sdk to zephyr as a module
    
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

---
+++ modules/modules.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) 2023 HPMicro
+# SPDX-License-Identifier: BSD-3-Clause
+
+set(ZEPHYR_HPM_SDK_CMAKE_DIR  ${CMAKE_CURRENT_LIST_DIR}/hpm_sdk)
+set(ZEPHYR_HPM_SDK_KCONFIG    ${CMAKE_CURRENT_LIST_DIR}/hpm_sdk/Kconfig)

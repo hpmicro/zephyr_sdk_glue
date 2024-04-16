2589b391ff2e8c3bd60a11365ce500c8720c2d50
Yingda WU <yingda.wu@hpmicro.com>
2024-04-16 14:19:58 +0800

    soc: add hpm63xx series files
    - add hpm63xx devicetree
    - add hpm63xx config
    - add hpm63xx linker file
    
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

---
+++ soc/riscv/hpmicro/hpm63xx/soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2022 hpmicro
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
+#ifndef __RISCV_HPMICRO_SOC_H_
+#define __RISCV_HPMICRO_SOC_H_
+
+#include <soc_common.h>
+#include <zephyr/devicetree.h>
+
+/* Machine timer memory-mapped registers */
+#define RISCV_MTIME_BASE             DT_REG_ADDR_BY_IDX(DT_NODELABEL(mtimer), 0)
+#define RISCV_MTIMECMP_BASE          DT_REG_ADDR_BY_IDX(DT_NODELABEL(mtimer), 1)
+
+#endif /* __RISCV_HPMICRO_SOC_H_ */

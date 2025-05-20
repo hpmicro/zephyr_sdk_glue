baf065402c19a6b4b4308b54992815b7e0ac4511
Yingda WU <yingda.wu@hpmicro.com>
2025-05-20 17:47:55 +0800

    [update] include:update clock controller
    - add soc clock node name in clock controller
    
    Signed-off-by: Yingda WU <yingda.wu@hpmicro.com>

--- include/drivers/clock_control/hpmicro_clock_control.h
+++ include/drivers/clock_control/hpmicro_clock_control.h
@@ -7,6 +7,17 @@
 #ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_HPMICRO_H_
 #define ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_HPMICRO_H_
 
+#if defined(CONFIG_SOC_SERIES_HPM67XX)
+#include <dt-bindings/clock/hpm6750-clocks.h>
+#elif defined(CONFIG_SOC_SERIES_HPM53XX)
+#include <dt-bindings/clock/hpm5361-clocks.h>
+#elif defined(CONFIG_SOC_SERIES_HPM62XX)
+#include <dt-bindings/clock/hpm6280-clocks.h>
+#elif defined(CONFIG_SOC_SERIES_HPM63XX)
+#include <dt-bindings/clock/hpm6360-clocks.h>
+#elif defined(CONFIG_SOC_SERIES_HPM68XX)
+#include <dt-bindings/clock/hpm6880-clocks.h>
+#endif /* CONFIG_SOC_SERIES_HPMXXXX */
 #include <zephyr/device.h>
 
 /**
